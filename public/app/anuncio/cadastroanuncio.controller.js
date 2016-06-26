e12616aceebc6f384c114600b2e219f2f222bd9d
Bruno Polone <bruno_soune@hotmail.com>
2016-06-26 19:37:27 -0300

    implementacao pagina cadastro anuncio cadastro usuario e login

--- public/app/anuncio/cadastroanuncio.controller.js
+++ public/app/anuncio/cadastroanuncio.controller.js
@@ -5,27 +5,61 @@
   function CadastroAnuncioCtrl($scope, $location) {
 
   	$scope.eventsWizard = function() {
-  		$("#tabs-anuncio a[aria-controls='geral']").on('click', function(){
-  			$("#geral").tab("show");
-  		});
-  		$("#tabs-anuncio a[aria-controls='localizacao']").on('click', function(){
-  			$("#localizacao").tab("show");
-  		});
-  		$("#tabs-anuncio a[aria-controls='espaco']").on('click', function(){
-  			$("#espaco").tab("show");
-  		});
-  		$("#tabs-anuncio a[aria-controls='comodidade']").on('click', function(){
-  			$("#comodidade").tab("show");
-  		});
-  		$("#tabs-anuncio a[aria-controls='ofertavalor']").on('click', function(){
-  			$("#ofertavalor").tab("show");
-  		});
-  		$("#tabs-anuncio a[aria-controls='preco']").on('click', function(){
-  			$("#preco").tab("show");
-  		});
-  		$("#tabs-anuncio a[aria-controls='fotos']").on('click', function(){
-  			$("#fotos").tab("show");
-  		});
+
+      $('#btnGravarGeral').on('click', function(){
+        $("#tabs-anuncio a[aria-controls='localizacao']").tab('show');
+      });
+
+
+      $('#btnGravarLocal').on('click', function(){
+        $("#tabs-anuncio a[aria-controls='espaco']").tab('show');
+      });
+
+      $('#btnVoltarLocal').on('click', function(){
+        $("#tabs-anuncio a[aria-controls='geral']").tab('show');
+      });
+
+
+      $('#btnGravarEspaco').on('click', function(){
+        $("#tabs-anuncio a[aria-controls='comodidade']").tab('show');
+      });
+
+      $('#btnVoltarEspaco').on('click', function(){
+        $("#tabs-anuncio a[aria-controls='localizacao']").tab('show');
+      });
+
+
+      $('#btnGravarComodidade').on('click', function(){
+        $("#tabs-anuncio a[aria-controls='ofertavalor']").tab('show');
+      });
+
+      $('#btnVoltarComodidade').on('click', function(){
+        $("#tabs-anuncio a[aria-controls='espaco']").tab('show');
+      });
+
+
+      $('#btnGravarOfertaValor').on('click', function(){
+        $("#tabs-anuncio a[aria-controls='preco']").tab('show');
+      });
+
+      $('#btnVoltarOfertaValor').on('click', function(){
+        $("#tabs-anuncio a[aria-controls='comodidade']").tab('show');
+      });
+
+
+      $('#btnGravarPreco').on('click', function(){
+        $("#tabs-anuncio a[aria-controls='fotos']").tab('show');
+      });
+
+      $('#btnVoltarPreco').on('click', function(){
+        $("#tabs-anuncio a[aria-controls='ofertavalor']").tab('show');
+      });
+
+
+      $('#btnVoltarFotos').on('click', function(){
+        $("#tabs-anuncio a[aria-controls='preco']").tab('show');
+      });
+
   	};
 
   	$scope.eventsWizard();
--- public/app/menu/menu.js
+++ public/app/menu/menu.js
@@ -3,9 +3,11 @@
 	function trocarLogo () {
 		var largJanela = window.innerWidth;
 		if (largJanela < 768) {
-			$('.img-logo').attr('src','app/img/casa-marrom-transparencia.png');
+			/*$('.img-logo').attr('src','app/img/casa-marrom-transparencia.png');*/
+			$('.img-logo').attr('src','app/img/logoverde.png');
 		}else {
-			$('.img-logo').attr('src','app/img/casa-marrom-transparencia.png');
+			/*$('.img-logo').attr('src','app/img/casa-marrom-transparencia.png');*/
+			$('.img-logo').attr('src','app/img/logobranca-casa.png');
 		}
 	}
 
