be91ad0539bf3478a917fc78f09b69f7459fdfac
Augusto <augusto.jm.amaral1@gmail.com>
2016-10-12 18:08:12 -0300

    Alteração Planos, População do Banco não mais necessario

--- libs/boot.js
+++ libs/boot.js
@@ -1,6 +1,6 @@
 module.exports = function(app) {
     app.listen(app.get('port'), () => {
-        app.libs.populatedb.run();
+        // app.libs.populatedb.run();
         console.log('CampusHouse Online - porta ' + app.get('port'));
     });
 };
